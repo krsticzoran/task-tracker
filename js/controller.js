b075d4744aea55cb9f137928a48ba77271d25efa
zorannkrstic <zorankrstic81@gmail.com>
2023-02-25 15:09:09 +0100

    addMarker

--- js/controller.js
+++ js/controller.js
@@ -504,12 +504,7 @@ navigator.geolocation.getCurrentPosition(
       maxZoom: 20,
       subdomains: ["mt0", "mt1", "mt2", "mt3"],
     }).addTo(map);
-    // L.tileLayer("https://{s}.tile.openstreetmap.fr/hot//{z}/{x}/{y}.png", {
-    //   attribution:
-    //     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    // }).addTo(map);
 
-    //marker();
     addMarker(input, L, map);
 
     currentPosition(latitude, longitude);
