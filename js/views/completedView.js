0f0196bc13f10a65e40152e178770fd06b5b0963
zorannkrstic <zorankrstic81@gmail.com>
2023-03-04 13:21:40 +0100

    map

--- js/views/completedView.js
+++ js/views/completedView.js
@@ -1,6 +1,6 @@
 export const completed = document.querySelector(".completed--list");
 export const completedView = function (inputOne) {
-  for (let i = 0; i < inputOne.length; i++) {
-    completed.innerHTML += `<li class='completed--li'><span class="span--completed">${inputOne[i][0]}</span><span class='date-for-delete span--to-do-date'>${inputOne[i][1]}</span><button class='delete'>Delete</button></li>`;
-  }
+  inputOne.map((inputOne) => {
+    completed.innerHTML += `<li class='completed--li'><span class="span--completed">${inputOne[0]}</span><span class='date-for-delete span--to-do-date'>${inputOne[1]}</span><button class='delete'>Delete</button></li>`;
+  });
 };
--- js/views/searchView.js
+++ js/views/searchView.js
@@ -1,8 +1,8 @@
 export const listSearch = document.querySelector(".search--list");
 
 export const searchView = function (temporary) {
-  for (let i = 0; i < temporary.length; i++) {
-    listSearch.innerHTML += `<li class="list--li"><input type="checkbox" class="check" /><span class="span--to-do">${temporary[i][0]}</span
-  ><span class='span--to-do-date'>${temporary[i][1]}</span><button class="btn--confirm">Confirm</button></li>`;
-  }
+  temporary.map((temporary) => {
+    listSearch.innerHTML += `<li class="list--li"><input type="checkbox" class="check" /><span class="span--to-do">${temporary[0]}</span
+    ><span class='span--to-do-date'>${temporary[1]}</span><button class="btn--confirm">Confirm</button></li>`;
+  });
 };
