29bfad0616998474a64b1c65d1f48ddc2d01e971
zorannkrstic <zorankrstic81@gmail.com>
2023-03-09 13:12:07 +0100

    filterFailedTask

--- app/models/inputDataModel.js
+++ app/models/inputDataModel.js
@@ -1,17 +1,38 @@
 class InputDataModel {
   static input = [];
+  static failed = [];
 
   static getInput() {
     return this.input;
   }
 
   static setInput() {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0); // set the time to 00:00:00:000 (midnight)
+
+    const localStorageInput = [];
+    //localStorage.clear("todo2");
+
+    JSON.parse(localStorage.getItem("todo2")) &&
+      this.failed.unshift(...JSON.parse(localStorage.getItem("todo2")));
+
     //localStorage.clear("todo");
-    const localStorageInput = JSON.parse(localStorage.getItem("todo"));
+    JSON.parse(localStorage.getItem("todo")) &&
+      localStorageInput.unshift(...JSON.parse(localStorage.getItem("todo")));
+    console.log(localStorageInput);
     if (localStorageInput) {
-      Array.isArray(localStorageInput)
-        ? this.input.unshift(...localStorageInput)
-        : this.input.unshift(localStorageInput);
+      const filteredFailed = localStorageInput.filter(function (task) {
+        return new Date(task.date).getTime() < today.getTime();
+      });
+      filteredFailed && this.failed.unshift(...filteredFailed);
+      localStorage.setItem("todo2", JSON.stringify(filteredFailed));
+      console.log(this.failed);
+      const filteredInput = localStorageInput.filter(function (task) {
+        return new Date(task.date).getTime() >= today.getTime();
+      });
+      filteredInput && this.input.unshift(...filteredInput);
+      localStorage.setItem("todo", JSON.stringify(this.input));
+      console.log(this.input);
     }
   }
 
